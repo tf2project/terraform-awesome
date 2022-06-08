a456fb53784e98655a3f175ed0f213d89fd17c3b
Saeid Bostandoust <49065072+ssbostan@users.noreply.github.com>
2022-06-08 23:41:07 +0100

    update main.tf file

--- examples/docker/easy-simple-volume-local/main.tf
+++ examples/docker/easy-simple-volume-local/main.tf
@@ -1,15 +1,10 @@
 # https://github.com/ssbostan/terraform-awesome
 
-resource "docker_volume" "foo" {
-  name   = "foo"
+resource "docker_volume" "simple_volume" {
+  name   = "simple_volume"
   driver = "local"
-  driver_opts = {
-    type : "btrfs"
-    device: "/dev/sda4"
-    o: "size=100m,uid=1000"
-  }
   labels {
-    label = "author"
+    label = "volume.docker.com/created-by"
     value = "Terraform Awesome"
   }
 }
