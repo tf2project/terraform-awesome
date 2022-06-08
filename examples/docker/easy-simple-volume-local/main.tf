e1b1e26643e76285f55d211f74efb2039eb55595
javad <heidarpoor2000@gmail.com>
2022-06-08 23:16:35 +0430

    add example:docker easy-simple-volume-local fixed

--- examples/docker/easy-simple-volume-local/main.tf
+++ examples/docker/easy-simple-volume-local/main.tf
@@ -1,21 +1,15 @@
 # https://github.com/ssbostan/terraform-awesome
 
-resource "docker_image" "redis_image" {
-  name = "redis:latest"
-}
-
-resource "docker_volume" "shared_volume" {
-  name = var.redis_volume_name
-}
-
-resource "docker_container" "redis_container" {
-  name  = "redis_container"
-  image = docker_image.redis_image.name
-
-  mounts {
-    source = docker_volume.shared_volume.name
-    target = "/data"
-    type   = "volume"
+resource "docker_volume" "foo" {
+  name   = "foo"
+  driver = "local"
+  driver_opts = {
+    type : "btrfs"
+    device: "/dev/sda4"
+    o: "size=100m,uid=1000"
+  }
+  labels {
+    label = "author"
+    value = "Terraform Awesome"
   }
 }
-
