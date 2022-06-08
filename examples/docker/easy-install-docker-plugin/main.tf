587e6d3b56049308f2d9cac7f78b0b92de87afcf
Saeid Bostandoust <49065072+ssbostan@users.noreply.github.com>
2022-06-09 00:03:44 +0100

    update file

--- examples/docker/easy-install-docker-plugin/main.tf
+++ examples/docker/easy-install-docker-plugin/main.tf
@@ -1,14 +1,9 @@
 # https://github.com/ssbostan/terraform-awesome
 
-resource "docker_plugin" "nfs-volume-plugin" {
+resource "docker_plugin" "docker_nfs_plugin" {
   name                  = "trajano/nfs-volume-plugin"
   alias                 = "nfs"
   enabled               = true
   grant_all_permissions = true
   force_destroy         = true
-  enable_timeout        = 60
-  force_disable         = true
-  env = [
-    "DEBUG=1"
-  ]
 }
