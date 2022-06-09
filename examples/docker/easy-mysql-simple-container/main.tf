29bbbfc06a0bf35d0ad31a2c44eaf5a4884e4150
mehran-torkaman <torkman.mehran@gmail.com>
2022-06-09 17:34:23 +0000

    add example:docker easy-mysql-simple-container

--- examples/docker/easy-mysql-simple-container/main.tf
+++ examples/docker/easy-mysql-simple-container/main.tf
@@ -1,16 +1,11 @@
 # https://github.com/ssbostan/terraform-awesome
 
-resource "random_pet" "mysql_name" {
-  separator = ""
-  length    = 1
-}
-
 resource "docker_image" "mysql_image" {
   name = "mysql:8"
 }
 
 resource "docker_container" "mysql_container" {
-  name  = "mysql-${random_pet.mysql_name.id}"
+  name  = "mysql"
   image = docker_image.mysql_image.name
   env   = toset(["MYSQL_ROOT_PASSWORD=root", "MYSQL_DATABASE=testing", "MYSQL_USER=test", "MYSQL_PASSWORD=test"])
   ports {
