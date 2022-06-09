45d34bf1834ff8425bc0f107f4ceea20323bf85b
Mohammad Hossein Salehinezhad <mohammadsalehi31335@gmail.com>
2022-06-09 11:04:48 +0430

    add example:docker easy-container-ulimit-options

---
+++ examples/docker/easy-container-ulimit-options/main.tf
@@ -0,0 +1,42 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "docker_image" "redis_image" {
+  name = "redis:latest"
+}
+
+resource "docker_container" "redis_container" {
+  name       = "redis_container"
+  image      = docker_image.redis_image.name
+  
+
+  ports {
+    internal = 6379
+    external = 6379
+  }
+
+  ulimit { 
+    name = "core"
+    soft = 1
+    hard = 2
+  }
+
+  ulimit {
+    name = "nofile"
+    soft = 20000
+    hard = 40000
+  }
+
+  ulimit {
+    name = "cpu"
+    soft = 2
+    hard = 3
+  }
+
+  ulimit {
+    
+    name = "memlock"
+    soft = 100
+    hard = 200
+  }
+
+}
