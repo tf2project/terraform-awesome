423efdbf3d012cd35b63ba1b38d77d2497d77688
Saeid Bostandoust <49065072+ssbostan@users.noreply.github.com>
2022-06-09 10:41:01 +0100

    Update main.tf

--- examples/docker/easy-container-ulimit-options/main.tf
+++ examples/docker/easy-container-ulimit-options/main.tf
@@ -5,38 +5,30 @@ resource "docker_image" "redis_image" {
 }
 
 resource "docker_container" "redis_container" {
-  name       = "redis_container"
-  image      = docker_image.redis_image.name
-  
-
+  name  = "redis_container"
+  image = docker_image.redis_image.name
   ports {
     internal = 6379
     external = 6379
   }
-
-  ulimit { 
+  ulimit {
     name = "core"
     soft = 1
     hard = 2
   }
-
   ulimit {
     name = "nofile"
     soft = 20000
     hard = 40000
   }
-
   ulimit {
     name = "cpu"
     soft = 2
     hard = 3
   }
-
   ulimit {
-    
     name = "memlock"
     soft = 100
     hard = 200
   }
-
 }
