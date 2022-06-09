a68ca0daf2e8a209962684787d2c420ca986e9db
M.Shahbazi <mohsenshahbazi@gmail.com>
2022-06-10 02:30:16 +0430

    add example:docker easy-container-custom-namespace

---
+++ examples/docker/easy-container-custom-namespace/main.tf
@@ -0,0 +1,15 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "docker_image" "nginx" {
+  name = "nginx"
+}
+
+resource "docker_container" "container_003" {
+  name        = "container_003"
+  image       = docker_image.nginx.latest
+  userns_mode = "terraformawesome:165536:65536"
+}
+
+
+
+
