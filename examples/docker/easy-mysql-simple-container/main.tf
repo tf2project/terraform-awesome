a5184cb83aab13c40940836d9eb5db57db998402
Saeid Bostandoust <49065072+ssbostan@users.noreply.github.com>
2022-06-09 17:42:01 +0100

    Update main.tf

--- examples/docker/easy-mysql-simple-container/main.tf
+++ examples/docker/easy-mysql-simple-container/main.tf
@@ -12,7 +12,12 @@ resource "docker_image" "mysql_image" {
 resource "docker_container" "mysql_container" {
   name  = "mysql-${random_pet.mysql_name.id}"
   image = docker_image.mysql_image.name
-  env   = toset(["MYSQL_ROOT_PASSWORD=root", "MYSQL_DATABASE=testing", "MYSQL_USER=test", "MYSQL_PASSWORD=test"])
+  env = [
+    "MYSQL_ROOT_PASSWORD=root",
+    "MYSQL_DATABASE=testing",
+    "MYSQL_USER=test",
+    "MYSQL_PASSWORD=test"
+  ]
   ports {
     internal = "3306"
     external = "3306"
