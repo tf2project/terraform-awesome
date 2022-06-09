f0daa4f2bec6984a15a3224ead276801c8a7512c
amir <amirnoshian1@gmail.com>
2022-06-09 14:23:52 +0430

    add example: docker easy-container-with-labels

---
+++ examples/docker/easy-container-with-labels/main.tf
@@ -0,0 +1,14 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "docker_image" "nginx_image" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_container"
+  image = docker_image.nginx_image.name
+  labels {
+    label = "volume.docker.com/created-by"
+    value = "Terraform Awesome"
+  }
+}
