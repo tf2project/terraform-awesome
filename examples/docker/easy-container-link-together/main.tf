cca02fbc00b21d85fac68c2dbc2a8f1854b34f1d
mehran-torkaman <torkman.mehran@gmail.com>
2022-06-09 12:15:52 +0000

    add example:docker easy-container-link-together

---
+++ examples/docker/easy-container-link-together/main.tf
@@ -0,0 +1,31 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "random_pet" "nginx_container_name" {
+  separator = ""
+  length = 1
+}
+
+resource "random_pet" "alpine_container_name" {
+  separator = ""
+  length = 1
+}
+
+resource "docker_image" "nginx_image" {
+  name = "nginx:latest"
+}
+
+resource "docker_image" "alpine_image" {
+  name = "alpine:latest"
+}
+
+resource "docker_container" "nginx_container" {
+  image = docker_image.nginx_image.name
+  name = "nginx-${random_pet.nginx_container_name.id}"
+}
+
+resource "docker_container" "alpine_container" {
+  image = docker_image.alpine_image.name
+  name = "alpine-${random_pet.alpine_container_name.id}"
+  links = toset(["${docker_container.nginx_container.name}"])
+  command = ["sleep", "1000"]
+}
