f0a9725c7bd72792d942b9163a83d73a63a1c454
saeed <saeedhosseini21@gmail.com>
2022-06-09 16:07:27 +0430

    add example:docker moderate-mysql-phpmyadmin-stack

---
+++ examples/docker/moderate-mysql-phpmyadmin-stack/main.tf
@@ -0,0 +1,38 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "docker_network" "backend" {
+  name     = "backend"
+  internal = true
+}
+
+resource "docker_network" "frontend" {
+  name = "frontend"
+}
+
+resource "docker_container" "mysql_container" {
+  name  = "mysql"
+  image = "mysql:latest"
+  env   = ["MYSQL_ROOT_PASSWORD=test", "MYSQL_DATABASE=test", "MYSQL_USER=test", "MYSQL_PASSWORD=test"]
+  networks_advanced {
+    name    = docker_network.backend.name
+    aliases = ["backend"]
+  }
+}
+
+resource "docker_container" "phpmyadmin_container" {
+  name  = "phpmyadmin"
+  image = "phpmyadmin/phpmyadmin"
+  env   = ["PMA_ARBITRARY=1", "PMA_HOST=${docker_container.mysql_container.name}", "PMA_USER=test", "PMA_PASSWORD=test"]
+  ports {
+    internal = 80
+    external = 8080
+  }
+  networks_advanced {
+    name    = docker_network.backend.name
+    aliases = ["backend"]
+  }
+  networks_advanced {
+    name    = docker_network.frontend.name
+    aliases = ["frontend"]
+  }
+}
