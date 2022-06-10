09ae75292ba9c657b34ea7bb994830be2a67c119
saeed <saeedhosseini21@gmail.com>
2022-06-10 16:53:20 +0430

    add example:docker moderate-custom-network-bridge-driver

---
+++ examples/docker/moderate-custom-network-bridge-driver/main.tf
@@ -0,0 +1,23 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "docker_network" "custom_network" {
+  name = "custom_network"
+  ipam_config {
+    ip_range = "172.28.5.0/24"
+    subnet   = "172.28.0.0/16"
+    gateway  = "172.28.5.254"
+  }
+}
+
+resource "docker_container" "alpine_container" {
+  name  = "alpine_container"
+  image = "alpine:latest"
+  command = [
+    "tail",
+    "-f",
+    "/dev/null"
+  ]
+  networks_advanced {
+    name = docker_network.custom_network.name
+  }
+}
