16a7d9f300a7be95263843b9256434e88112f44f
mrunix1998 <mohammadsalehi31335@gmail.com>
2022-06-11 12:57:22 +0430

    add examples:moderate-nginx-https-letsencrypt-certificate

--- examples/docker/moderate-nginx-https-letsencrypt-certificate/certificate.tf
+++ examples/docker/moderate-nginx-https-letsencrypt-certificate/certificate.tf
@@ -14,30 +14,11 @@ resource "acme_certificate" "certificate" {
   account_key_pem           = acme_registration.reg.account_key_pem
   common_name               = "www.devops.com"
   subject_alternative_names = ["devops.com"]
-
   http_challenge {
     port         = "80"
     proxy_header = "Forwarded"
   }
-
 }
-
-
-
-
-
-# resource "tls_self_signed_cert" "nginx_https_tls_cert" {
-#   private_key_pem       = tls_private_key.nginx_https_tls_key.private_key_pem
-#   validity_period_hours = 720
-#   allowed_uses = [
-#     "any_extended"
-#   ]
-#   dns_names = ["localhost"]
-#   subject {
-#     common_name  = "localhost"
-#     organization = "Terraform Awesome"
-#   }
-# }
 
 resource "local_file" "nginx_https_tls_key" {
   filename = "tls.key"
--- examples/docker/moderate-nginx-https-letsencrypt-certificate/providers.tf
+++ examples/docker/moderate-nginx-https-letsencrypt-certificate/providers.tf
@@ -6,5 +6,4 @@ provider "docker" {
 
 provider "acme" {
   server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
-
 }
