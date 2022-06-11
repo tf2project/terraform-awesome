d5f5529eda809d58eaadf87acd6004657658e6b4
M.Shahbazi <mohsenshahbazi@gmail.com>
2022-06-11 12:09:21 +0430

    add example:docker moderate-pull-private-image-container change ipc-net-pid mode

--- examples/docker/easy-container-custom-namespace/main.tf
+++ examples/docker/easy-container-custom-namespace/main.tf
@@ -5,9 +5,12 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "container_003" {
-  name        = "container_003"
-  image       = docker_image.nginx.latest
-  userns_mode = "terraformawesome:165536:65536"
+  name         = "container_003"
+  image        = docker_image.nginx.latest
+  userns_mode  = "terraformawesome:165536:65536"
+  pid_mode     = "host"
+  ipc_mode     = "host"
+  network_mode = "host"
 }
 
 
