9cf6bae57fc9018349f1ec85953b2027b3836f5c
mohammadamirpanah <amirpanah.mohammad2@gmail.com>
2022-06-13 09:59:04 -0400

    update to add example:docker moderate-wordpress-stack-production

--- examples/docker/moderate-wordpress-stack-production/main.tf
+++ examples/docker/moderate-wordpress-stack-production/main.tf
@@ -29,6 +29,10 @@ resource "docker_container" "mysql_container" {
     name = docker_network.backend.name
   }
   volumes {
+    host_path      = abspath("wordpress/conf")
+    container_path = "/etc/mysql/conf.d"
+  }
+  volumes {
     volume_name    = docker_volume.mysql_data.name
     container_path = "/var/lib/mysql"
   }
@@ -53,8 +57,39 @@ resource "docker_container" "wordpress_container" {
     name = docker_network.backend.name
   }
   volumes {
+    host_path      = abspath("wordpress/uploads.ini")
+    container_path = "/usr/local/etc/php/conf.d/uploads.ini"
+  }
+  volumes {
     volume_name    = docker_volume.wordpress_data.name
     container_path = "/var/www/html"
   }
   depends_on = [docker_container.mysql_container]
+}
+resource "docker_container" "nginx_container" {
+  name    = "webserver"
+  image   = "nginx:1.15.12-alpine"
+  restart = "always"
+  env = [
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_ROOT_PASSWORD=somewordpress",
+    "MYSQL_USER=wordpress",
+    "MYSQL_PASSWORD=wordpress"
+  ]
+  networks_advanced {
+    name = docker_network.backend.name
+  }
+  ports {
+    internal = "80"
+    external = "80"
+  }
+  volumes {
+    host_path      = abspath("wordpress/nginx-conf")
+    container_path = "/etc/nginx/conf.d"
+  }
+  volumes {
+    volume_name    = docker_volume.wordpress_data.name
+    container_path = "/var/www/html"
+  }
+  depends_on = [docker_container.wordpress_container]
 }
