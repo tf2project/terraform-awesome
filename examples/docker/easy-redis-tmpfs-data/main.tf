e08270a0863a3d8c983a397de823b6d1f02a14e7
javad <heidarpoor2000@gmail.com>
2022-06-13 13:03:23 +0430

    add example:docker easy-redis-tmpfs-data fixed

--- examples/docker/easy-redis-tmpfs-data/main.tf
+++ examples/docker/easy-redis-tmpfs-data/main.tf
@@ -3,19 +3,6 @@
 resource "docker_image" "redis_image" {
   name = "redis:latest"
 }
-resource "docker_volume" "simple_tmpfs_volume" {
-  name   = "simple_tmpfs_volume"
-  driver = "local"
-  driver_opts = {
-    type : "tmpfs"
-    device : "tmpfs"
-    o : "size=100m,uid=1000"
-  }
-  labels {
-    label = "volume.docker.com/created-by"
-    value = "Terraform Awesome"
-  }
-}
 resource "docker_container" "redis_container" {
   name    = "redis_container"
   image   = docker_image.redis_image.name
@@ -28,14 +15,26 @@ resource "docker_container" "redis_container" {
   memory     = 1024
   cpu_shares = 2
 
-  volumes {
-    volume_name    = docker_volume.simple_tmpfs_volume.name
-    container_path = "/data"
-    read_only      = false
-  }
   labels {
     label = "container.docker.com/created-by"
     value = "Terraform Awesome"
   }
-}
 
+  mounts {
+    target    = "/data"
+    type      = "tmpfs"
+    read_only = false
+    volume_options {
+      driver_name = "name"
+      driver_options = {
+        type : "tmpfs"
+        device : "tmpfs"
+        o : "size=100m,uid=1000"
+      }
+      labels {
+        label = "volume.docker.com/created-by"
+        value = "Terraform Awesome"
+      }
+    }
+  }
+}
