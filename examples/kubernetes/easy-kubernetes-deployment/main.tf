65cbb19becb0351658b80f0ccf46d68d6d0d3362
mehran <torkman.mehran@gmail.com>
2022-06-13 13:08:50 +0430

    add example:kubernetes easy-kubernetes-deployment

---
+++ examples/kubernetes/easy-kubernetes-deployment/main.tf
@@ -0,0 +1,37 @@
+resource "kubernetes_deployment" "nginx_deployment" {
+  metadata {
+    name = "nginx"
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
