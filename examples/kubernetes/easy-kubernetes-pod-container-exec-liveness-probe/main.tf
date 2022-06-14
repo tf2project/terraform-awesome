ae38b9bccb34ead97067d97d5a76c18bf64621e2
mohammad hossein salehinezhad <salehimohammad331@gmail.com>
2022-06-14 10:18:06 +0430

    add example:kubernetes easy-kubernetes-pod-container-exec-liveness-probe

---
+++ examples/kubernetes/easy-kubernetes-pod-container-exec-liveness-probe/main.tf
@@ -0,0 +1,38 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "kubernetes_pod" "nginx" {
+  metadata {
+    name      = "nginx"
+    namespace = "default"
+    labels = {
+      "app.kubernetes.io/name"       = "nginx"
+      "app.kubernetes.io/created-by" = "terraform-awesome"
+    }
+  }
+  spec {
+    container {
+      name  = "nginx"
+      image = "nginx:latest"
+      readiness_probe {
+        http_get {
+          path = "/"
+          port = 80
+        }
+        initial_delay_seconds = 30
+        timeout_seconds = 10
+        period_seconds = 10
+        failure_threshold = 3
+      }
+      port {
+        name           = "http"
+        protocol       = "TCP"
+        container_port = 80
+      }
+      port {
+        name           = "https"
+        protocol       = "TCP"
+        container_port = 443
+      }
+    }
+  }
+}
