78cdcce474d23f3ba57879af5b812a396150465c
Saeid Bostandoust <49065072+ssbostan@users.noreply.github.com>
2022-06-14 11:51:44 +0100

    Update main.tf

--- examples/tls/easy-csr-with-new-private-key/main.tf
+++ examples/tls/easy-csr-with-new-private-key/main.tf
@@ -5,24 +5,21 @@ resource "tls_private_key" "private_key" {
 }
 
 resource "local_file" "private_key" {
-  filename        = "private_key.pem"
+  filename        = "tls.key"
   content         = tls_private_key.private_key.private_key_pem
   file_permission = "0600"
 }
 
-resource "tls_cert_request" "csr_file" {
-  private_key_pem = local_file.private_key.content
-
+resource "tls_cert_request" "tls_csr_request" {
+  private_key_pem = tls_private_key.private_key.private_key_pem
   subject {
     common_name  = "example.com"
     organization = "terraform-awesome"
   }
 }
 
-
-resource "local_file" "csr_file" {
-  filename        = "csr_file.txt"
-  content         = tls_cert_request.csr_file.cert_request_pem
+resource "local_file" "tls_csr_request" {
+  filename        = "tls.csr"
+  content         = tls_cert_request.tls_csr_request.cert_request_pem
   file_permission = "0600"
 }
-
