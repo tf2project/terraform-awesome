2aa67e4215d08d0e2e1c089ef3d9a43713a98f16
ahmad <ahmadmalekiha@gmail.com>
2022-06-14 12:08:45 +0430

    add example:kubernetes easy-kubernetes-pod-container-exec-startup-probe

---
+++ examples/kubernetes/easy-kubernetes-pod-container-exec-startup-probe/main.tf
@@ -0,0 +1,29 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "kubernetes_pod" "busybox_startup" {
+  metadata {
+    name      = "busyboxstartup"
+    namespace = "default"
+    labels = {
+      "app.kubernetes.io/name"       = "busyboxstartup"
+      "app.kubernetes.io/created-by" = "terraform-awesome"
+    }
+  }
+  spec {
+    container {
+      name  = "busyboxstartup"
+      image = "k8s.gcr.io/busybox"
+      args = ["/bin/sh", "-c", "touch /tmp/healthy; sleep 30; rm -f /tmp/healthy; sleep 600"]
+      startup_probe {
+        initial_delay_seconds = 5
+        period_seconds = 5
+        timeout_seconds = 1
+        failure_threshold = 3
+        success_threshold = 1
+        exec {
+          command = ["cat", "/tmp/healthy"]
+        }
+      }
+    }
+  }
+}
