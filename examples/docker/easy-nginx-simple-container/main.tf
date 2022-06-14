24b60b606f42bf2dc5d4a3a8f916951b655622c5
Saeid Bostandoust <49065072+ssbostan@users.noreply.github.com>
2022-06-14 11:59:17 +0100

    Update main.tf

--- examples/docker/easy-nginx-simple-container/main.tf
+++ examples/docker/easy-nginx-simple-container/main.tf
@@ -8,10 +8,6 @@ resource "docker_container" "nginx_container" {
   name   = "nginx_container"
   image  = docker_image.nginx_image.name
   memory = 1024
-  volumes {
-    host_path      = abspath("nginx.conf")
-    container_path = "/etc/nginx/nginx.conf"
-  }
   ports {
     internal = "80"
     external = "80"
