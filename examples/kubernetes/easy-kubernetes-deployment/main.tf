cd5944c0cfc83d62ede8aaaf2df18d4d01ff83d7
mehran <torkman.mehran@gmail.com>
2022-06-15 00:19:15 +0430

    add example:kubernetes easy-kubernetes-deployment

---
+++ examples/kubernetes/easy-kubernetes-deployment/main.tf
@@ -0,0 +1,41 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "kubernetes_deployment" "nginx_deployment" {
+  metadata {
+    name      = "nginx"
+    namespace = "default"
+    labels = {
+      "app.kubernetes.io/name"       = "nginx"
+      "app.kubernetes.io/created-by" = "terraform-awesome"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
