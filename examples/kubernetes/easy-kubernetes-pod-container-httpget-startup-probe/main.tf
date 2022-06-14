e43190e9e0dd18640a7a3015f2f41aa9dac66b86
ahmad <ahmadmalekiha@gmail.com>
2022-06-14 14:22:13 +0430

    add example:kubernetes easy-kubernetes-pod-container-httpget-startup-probe

---
+++ examples/kubernetes/easy-kubernetes-pod-container-httpget-startup-probe/main.tf
@@ -0,0 +1,39 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "kubernetes_pod" "startuphttp" {
+  metadata {
+    name      = "startuphttp"
+    namespace = "default"
+    labels = {
+      "app.kubernetes.io/name"       = "startuphttp"
+      "app.kubernetes.io/created-by" = "terraform-awesome"
+    }
+  }
+  spec {
+    container {
+      name  = "startuphttp"
+      image = "k8s.gcr.io/liveness"
+      args  = ["/server"]
+      port {
+        name           = "startup-port"
+        container_port = "8080"
+        host_port      = "8080"
+      }
+      liveness_probe {
+        initial_delay_seconds = 3
+        period_seconds        = 10
+        timeout_seconds       = 1
+        failure_threshold     = 30
+        success_threshold     = 1
+        http_get {
+          path = "/healthz"
+          port = "startup-port"
+          http_header {
+            name  = "Custom-Header"
+            value = "Awesome"
+          }
+        }
+      }
+    }
+  }
+}
