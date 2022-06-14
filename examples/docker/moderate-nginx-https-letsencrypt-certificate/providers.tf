2a4a5c43c93628912eb7e1f75631895984f0bd4c
mohammad hossein salehinezhad <salehimohammad331@gmail.com>
2022-06-14 15:01:03 +0430

    add example:kubernetes easy-kubernetes-pod-container-httpget-liveness-probe

--- examples/docker/moderate-nginx-https-letsencrypt-certificate/providers.tf
+++
@@ -1,9 +0,0 @@
-# https://github.com/ssbostan/terraform-awesome
-
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
-provider "acme" {
-  server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
-}
