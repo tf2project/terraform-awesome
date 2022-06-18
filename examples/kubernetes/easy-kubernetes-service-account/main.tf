6368e667eecd3cc2acf670ccec5b85c359df8416
RArmandeh <roshanak.a.r@gmail.com>
2022-06-18 13:00:37 +0430

    add example:kubernetes easy-kubernetes-pod-container-resource-fields-env-variables

---
+++ examples/kubernetes/easy-kubernetes-service-account/main.tf
@@ -0,0 +1,22 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "kubernetes_service_account" "example" {
+  metadata {
+    name      = "example"
+    namespace = "default"
+    labels = {
+      "app.kubernetes.io/name"       = "nginx"
+      "app.kubernetes.io/created-by" = "terraform-awesome"
+    }
+  }
+  secret {
+    name = kubernetes_secret.example.metadata.0.name
+  }
+}
+
+resource "kubernetes_secret" "example" {
+  metadata {
+    name = "terraform-example"
+  }
+}
+
