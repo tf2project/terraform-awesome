78d8d7ab3248ac04dd0b77f47f77b9675487959a
s10i <s10i@localhost.localdomain>
2022-06-18 11:35:37 +0430

    add example:docker moderate-import-existing-docker-plugin

---
+++ examples/docker/moderate-import-existing-docker-plugin/main.tf
@@ -0,0 +1,9 @@
+# https://github.com/ssbostan/terraform-awesome
+
+resource "docker_plugin" "sample-volume-plugin" {
+  name                  = "tiborvass/sample-volume-plugin"
+  alias                 = "sample-volume-plugin"
+  enabled               = false
+  grant_all_permissions = true
+  force_destroy         = true
+}
