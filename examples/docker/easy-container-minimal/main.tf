d2422ef26f70cafc3ca90bf46d12b63e7ccd5626
Saeid Bostandoust <ssbostan@yahoo.com>
2022-06-19 19:55:16 +0100

    update example:docker easy-container-minimal

--- examples/docker/easy-container-minimal/main.tf
+++ examples/docker/easy-container-minimal/main.tf
@@ -1,7 +1,6 @@
 # https://github.com/ssbostan/terraform-awesome
 
 resource "docker_container" "simple_container" {
-  name     = "simple_container"
-  image    = "hello-world:latest"
-  must_run = false
+  name  = "simple_container"
+  image = "nginx:latest"
 }
