a30c9f3b5fc551ffaa7093afdf63728edfc2e24b
Saeid Bostandoust <ssbostan@yahoo.com>
2022-06-19 19:53:32 +0100

    update example:docker easy-container-linux-capabilities

--- examples/docker/easy-container-linux-capabilities/main.tf
+++ examples/docker/easy-container-linux-capabilities/main.tf
@@ -1,14 +1,11 @@
 # https://github.com/ssbostan/terraform-awesome
 
-resource "docker_image" "nginx_image" {
-  name = "nginx:latest"
-}
-
-resource "docker_container" "nginx_container" {
-  name  = "nginx_container"
-  image = docker_image.nginx_image.name
+resource "docker_container" "alpine_container" {
+  name    = "alpine_container"
+  image   = "alpine:latest"
+  command = ["ping", "google.com"]
   capabilities {
-    add  = ["CAP_NET_RAW", "CAP_SYS_TIME", "CAP_CHOWN", "CAP_SETGID", "CAP_SETUID"]
+    add  = ["CAP_NET_RAW"]
     drop = ["ALL"]
   }
 }
