14d8518825b98b2f162471637ab00b3c7e309828
Saeid Bostandoust <ssbostan@yahoo.com>
2022-06-20 12:47:16 +0100

    update example:docker easy-volume-local-driver

--- examples/docker/easy-volume-local-driver/main.tf
+++ examples/docker/easy-volume-local-driver/main.tf
@@ -4,7 +4,7 @@ resource "docker_volume" "simple_volume" {
   name   = "simple_volume"
   driver = "local"
   labels {
-    label = "volume.docker.com/created-by"
+    label = "volume.docker.io/created-by"
     value = "Terraform Awesome"
   }
 }
