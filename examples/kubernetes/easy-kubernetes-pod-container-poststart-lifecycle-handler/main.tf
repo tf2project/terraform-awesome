5f847e329b64d32dc32bdb56c1f798006f586170
unknown <mohammadhassanpour62@gmail.com>
2022-06-21 12:36:05 +0430

    add example:kubernetese easy-kubernetes-pod-container-poststart-lifecycle-handler

--- examples/kubernetes/easy-kubernetes-pod-container-poststart-lifecycle-handler/main.tf
+++ examples/kubernetes/easy-kubernetes-pod-container-poststart-lifecycle-handler/main.tf
@@ -8,29 +8,14 @@ resource "kubernetes_pod_container_postart" "nginx" {
     }
   }
   spec {
-    selector = {
-      "app.kubernetes.io/name"       = "nginx"
-      "app.kubernetes.io/created-by" = "terraform-awesome"
-    }
-    replicas = 1
-    template {
-      metadata {
-        labels = {
-          "app.kubernetes.io/name"       = "nginx"
-          "app.kubernetes.io/created-by" = "terraform-awesome"
-        }
-      }
-      spec {
-        container {
-          name  = "nginx"
-          image = "nginx:latest"
-          lifecycle:
-            postStart:
-              exec:
-                command: ["/bin/sh", "-c", "echo Hello from the postStart handler > /usr/share/message"]
-            preStop:
-              exec:
-                command: ["/bin/sh","-c","nginx -s quit; while killall -0 nginx; do sleep 1; done"]
+    container {
+      name  = "nginx"
+      image = "nginx:latest"
+      lifecycle {
+        post_start {
+          exec {
+            command = ["/bin/sh", "-c", "echo Hello from the postStart handler > /usr/share/message"]
+          }
         }
       }
     }
