7f3642a8bf676f7f3cddd2298781b3eec8d59c7c
Roshanak Armandeh <95270915+RArmandeh@users.noreply.github.com>
2022-06-29 15:17:01 +0430

    Update main.tf
    add example:kubernetes easy-kubernetes-storage-class-gce-persistent-disk

--- examples/kubernetes/easy-kubernetes-storage-class-gce-persistent-disk/main.tf
+++ examples/kubernetes/easy-kubernetes-storage-class-gce-persistent-disk/main.tf
@@ -2,16 +2,16 @@
 
 resource "kubernetes_storage_class" "example_gce_pd" {
   metadata {
-    name      = "terraform-example"
+    name = "terraform-example"
     labels = {
       "app.kubernetes.io/name"       = "example_gce_pd"
       "app.kubernetes.io/created-by" = "terraform-awesome"
     }
   }
   storage_provisioner = "kubernetes.io/gce-pd"
-  reclaim_policy      = "Retain"
   parameters = {
-    type = "pd-standard"
+    type             = "pd-standard"
+    fstype           = "ext4"
+    replication_type = "none"
   }
-  mount_options = ["file_mode=0700", "dir_mode=0777", "mfsymlinks", "uid=1000", "gid=1000", "nobrl", "cache=none"]
 }
