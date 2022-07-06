bfbd2b5eeddceab2d879fda9db5209fbe4fdf37a
Peymansafiri <peyman_safiri@yahoo.com>
2022-07-06 14:12:29 +0430

    Delete main.tf

--- examples/github/easy-github-repository-create-pull-request/main.tf
+++
@@ -1,9 +0,0 @@
-resource "github_repository_pull_request" "example" {
-  base_repository = var.github_repository
-  base_ref        = "main"
-  head_ref        = "feature-branch"
-  title           = "My newest feature"
-  body            = "This will change everything"
-}
-
-
