e2a35d0958fe24ab8bf288db5436da568ad0aadc
razzaghi.b@gmail.com <razzaghi.b@gmail.com>
2022-08-06 18:50:13 +0430

    add example:easy-kubernetes-persistent-volume-iscsi

---
+++ examples/kubernetes/easy-kubernetes-create-storage-class-for-iscsi/main.tf
@@ -0,0 +1,21 @@
+resource "kubenetes_persistent_volume_v1" "iscsi_volume" {
+    metadata {
+        name = "iscsi-volume"
+    }
+
+    iscsi {
+        fs_type = "ext4"
+        iqn = "iqn.2017-10.local.example.server:disk1"
+        read_only = false
+        target_portal = 192.168.1.1
+    }
+
+    spec {
+        capacity = {
+            storage = "2Gi"
+        }
+
+        access_modes = ["ReasWriteMnay"]
+
+    }
+}
